ee2c08a0aaa2e134e6646af583d072a04934058f
bernhard <bz@datenkueche.com>
2015-08-08 11:49:23 +0200

    README, what is it for

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # opengraph-proxy
+
+Example usage
+
+```
+http://localhost:3000/?url=https://www.youtube.com/watch?v=kQypE-WMXAU
+```
+
+The angular client has opengraph support. At buddycloud.org it does not work because the 
+opengraph is loaded from here:
+```
+http://datenkueche.com/buddycloud/ogp/crawler.php?url=https://www.youtube.com/watch?v=kQypE-WMXAUhttp://datenkueche.com/buddycloud/ogp/crawler.php?url=https://www.youtube.com/watch?v=kQypE-WMXAU
+```
+This is not https and the browser does not allow it.
+
+The script must be installed at buddycloud.org.
---
+++ og.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var og = require('open-graph');
+
+var allowCrossDomain = function(req, res, next) {
+    //res.header('Access-Control-Allow-Origin', 'buddycloud.org');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+var app = express();
+app.use(allowCrossDomain);
+
+
+var url = "http://github.com/samholmes/node-open-graph/raw/master/test.html";
+
+app.get('/', function (req, res) {
+og(req.query.url, function(err, meta){
+    if(err){
+        res.send(err);
+    }else{
+        res.json(meta);
+        res.end();
+    }
+})
+
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+
+
+
+og(url, function(err, meta){
+    console.log(meta);
+})
